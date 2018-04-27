6aefdb2614d0391c45087cb9e0296308c1cb229b
Muhammad Fadhlan Supriadi <fadhlansupriadi@gmail.com>
2018-04-28 06:25:17 +0700

    Layout 0.3 Tampilan Awal - Lupa Sandi
    activity_lupa_password.xml

--- app/src/main/java/id/co/venport/vendorportalandroidapplication/TampilanAwalActivity.java
+++ app/src/main/java/id/co/venport/vendorportalandroidapplication/TampilanAwalActivity.java
@@ -3,15 +3,9 @@ package id.co.venport.vendorportalandroidapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class TampilanAwalActivity extends AppCompatActivity {
@@ -31,7 +25,7 @@ public class TampilanAwalActivity extends AppCompatActivity {
         auth=FirebaseAuth.getInstance();
 
         daftarButton =(Button)findViewById(R.id.daftarButton);
-        masukButton =(Button)findViewById(R.id.masukButton);
+        masukButton =(Button)findViewById(R.id.loginButton);
         fbButton =(Button)findViewById(R.id.fbButton);
         googleButton =(Button)findViewById(R.id.googleButton);
 
@@ -39,7 +33,7 @@ public class TampilanAwalActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent( TampilanAwalActivity.this,TampilanAwalActivity.class);
+                Intent intent = new Intent( TampilanAwalActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
         });
