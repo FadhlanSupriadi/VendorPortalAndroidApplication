8ba602006f1e3e74f49c873a81751e0d17ccd8fa
Muhammad Fadhlan Supriadi <fadhlansupriadi@gmail.com>
2018-05-14 12:21:59 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/FadhlanSupriadi/VendorPortalAndroidApplication""
    This reverts commit 36cbad92f324f468872dd3607a699a6bcac8c6c4.

--- app/src/main/java/id/co/venport/vendorportalandroidapplication/SplashLoginRegister/TampilanAwalActivity.java
+++ app/src/main/java/id/co/venport/vendorportalandroidapplication/SplashLoginRegister/TampilanAwalActivity.java
@@ -18,8 +18,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import id.co.venport.vendorportalandroidapplication.AppConfig.PreferenceIntro;
+import id.co.venport.vendorportalandroidapplication.AppConfig.PreferenceLogin;
 import id.co.venport.vendorportalandroidapplication.HomePageActivity;
 import id.co.venport.vendorportalandroidapplication.R;
+import id.co.venport.vendorportalandroidapplication.mainmenu.MainActivity;
 
 public class TampilanAwalActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -27,6 +30,7 @@ public class TampilanAwalActivity extends AppCompatActivity implements View.OnCl
     private FirebaseAuth firebaseAuth;
     private EditText user, passwd;
     private ProgressDialog progressDialog;
+    private PreferenceLogin preferenceLogin;
 
 
     @Override
@@ -35,6 +39,7 @@ public class TampilanAwalActivity extends AppCompatActivity implements View.OnCl
         setContentView(R.layout.activity_tampilan_awal);
 
         firebaseAuth=FirebaseAuth.getInstance();
+        preferenceLogin = new PreferenceLogin(this);
 
         progressDialog = new ProgressDialog(this);
 
@@ -54,8 +59,9 @@ public class TampilanAwalActivity extends AppCompatActivity implements View.OnCl
                 View mView = getLayoutInflater().inflate(R.layout.activity_dialog_login, null);
 
                 if(firebaseAuth.getCurrentUser()!=null){
+                    preferenceLogin.createIntro(firebaseAuth.getCurrentUser().getEmail());
                     finish();
-                    startActivity(new Intent(getApplicationContext(), HomePageActivity.class));
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 }
 
                 user = (EditText) mView.findViewById(R.id.username);
@@ -98,7 +104,7 @@ public class TampilanAwalActivity extends AppCompatActivity implements View.OnCl
     }
 
     private void userLogin(){
-        String email = user.getText().toString().trim();
+        final String email = user.getText().toString().trim();
         String password = passwd.getText().toString().trim();
 
         if(TextUtils.isEmpty(email)){
@@ -122,8 +128,9 @@ public class TampilanAwalActivity extends AppCompatActivity implements View.OnCl
             public void onComplete(@NonNull Task<AuthResult> task) {
                 progressDialog.dismiss();
                 if(task.isSuccessful()){
+                    preferenceLogin.createIntro(email);
                     finish();
-                    startActivity(new Intent(getApplicationContext(), HomePageActivity.class));
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 }else{
                     Toast.makeText(TampilanAwalActivity.this,"Maybe user or password is wrong",Toast.LENGTH_LONG).show();
                 }
